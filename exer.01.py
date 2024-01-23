12753c194f825f58ed24c8e92fa4f6e670f40d16
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-01-23 17:39:50 -0300

    :build: :test: < Ajuste de config para pipeline e codigos para apagar pastas antes de rodar codigos>

--- exer.01.py
+++ exer.01.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 # Caminho da pasta exer.01
-folder_path = os.getenv("EXER_FILES_PATH","exer.01")
+folder_path = os.getenv("EXER_FILES_PATH", "exer.01")
 
 # Remove todos os arquivos na pasta exer.01
 for filename in os.listdir(folder_path):
@@ -18,30 +18,31 @@ for filename in os.listdir(folder_path):
 url = "https://jsonplaceholder.typicode.com/posts"
 
 counter = 1
-while url != None:
-    payload = {}
-    headers = {}
+while url:
+    try:
+        response = requests.get(url).json()
 
-    response = json.loads(requests.request("GET", url, headers=headers, data=payload).text)
+        for post in response:
+            info = {
+                "userID": post["userId"],
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            }
 
-    for post in response:
-        info = {
-            "userID": post["userId"],
-            "id": post["id"],
-            "title": post["title"],
-            "body": post["body"]
-        }
+            print(post["id"])
 
-        print(post["id"])
+            # Aqui, mudamos o caminho do arquivo para incluir o ID do post no nome do arquivo
+            file_path = os.path.join(folder_path, f"post_{post['id']}.json")
 
-        # Aqui, mudamos o caminho do arquivo para incluir o ID do post no nome do arquivo
-        file_path = os.path.join(folder_path, f"post_{post['id']}.json")
+            # Salvamos cada post individualmente em um arquivo separado
+            with open(file_path, "w") as outfile:
+                print(f"Salvando arquivo em: {file_path}")
+                json.dump(info, outfile, indent=2)
 
-        # Salvamos cada post individualmente em um arquivo separado
-        with open(file_path, "w") as outfile:
-            print(f"Salvando arquivo em: {file_path}")
-            json.dump(info, outfile)
+        # Atualizamos a URL para a próxima página de resultados
+        url = response[-1]["id"] + 1 if response else None
+        counter += 1
 
-    # Atualizamos a URL para a próxima página de resultados
-    url = response[-1]["id"] + 1 if response else None
-    counter += 1
+    except Exception as e:
+        print(f"Erro durante a solicitação ou salvamento: {e}")
